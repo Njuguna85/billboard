0431e2ecc23522c062f487b73a1b3a05012a7883
Njuguna85 <dennix.njuguna85@gmail.com>
2020-07-05 23:07:29 +0300

    use https

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ async function fetchData() {
     // await response for the fetch call
     // since there are no options passed, it will be a GET request
     let response = await fetch('./php/download.php');
-    const url = 'http://bi.predictiveanalytics.co.ke/api/all-deliveries?start=03-07-2020&end=03-07-2020'
+    const url = 'https://bi.predictiveanalytics.co.ke/api/all-deliveries?start=03-07-2020&end=03-07-2020'
     let response2 = await fetch(url, {
         method: "GET",
         headers: {
@@ -36,8 +36,6 @@ function createMap(data) {
     const mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
     //
     // map styles
-    //'streets-v11'
-    //'light-v10'
     const streets = L.tileLayer(mapboxUrl, {
         id: 'light-v10',
         tileSize: 512,
