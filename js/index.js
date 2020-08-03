10fa9cf0c4923664b19b9630125e7f08601b40f9
Njuguna85 <dennix.njuguna85@gmail.com>
2020-08-04 00:37:48 +0300

    remove aq

--- js/index.js
+++ js/index.js
@@ -116,20 +116,19 @@ async function fetchMobileUploads() {
 }
 
 function addOverlays(data) {
-    // addBillboards(data.billboard);
-    addAQ(data.aq);
-    // addAtm(data.atm);
-    // addTrafficLayer();
-    // addNssf(data.nssf);
-    // addUber(data.uber);
-    // addSubLocations(data.sublocation);
+    addBillboards(data.billboard);
+    addAtm(data.atm);
+    addTrafficLayer();
+    addNssf(data.nssf);
+    addUber(data.uber);
+    addSubLocations(data.sublocation);
     // addugPopProj(data.ugPopProj);
-    // addGhanaPopulation(data.ghanaDistrictPopPulation);
-    // for (const [key, value] of Object.entries(data)) {
-    //     if (commD.includes(key)) {
-    //         add(key, value);
-    //     }
-    // }
+    addGhanaPopulation(data.ghanaDistrictPopPulation);
+    for (const [key, value] of Object.entries(data)) {
+        if (commD.includes(key)) {
+            add(key, value);
+        }
+    }
 }
 
 function addTrafficLayer() {
